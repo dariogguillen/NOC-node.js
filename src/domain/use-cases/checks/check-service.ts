1a39221f36d41b3045957e3ab919ca66c983468e
Darío Guillén <guillendario@gmail.com>
2025-04-21 20:44:50 -0600

    feat: creating filesystem logs

--- src/domain/use-cases/checks/check-service.ts
+++ src/domain/use-cases/checks/check-service.ts
@@ -1,3 +1,6 @@
+import { LogEntity, LogLevel } from "../../entities/log.entity";
+import { LogRepository } from "../../respository/log.repository";
+
 interface CheckServiceUserCase {
   execute(url: string): Promise<boolean>;
 }
@@ -8,6 +11,7 @@ type ErrorCallback = (error: string) => void;
 export class CheckService implements CheckServiceUserCase {
   // dependency injection
   constructor(
+    private readonly logRepository: LogRepository,
     private readonly successCallback: SuccessCallback,
     private readonly errorCallback: ErrorCallback,
   ) {}
@@ -24,11 +28,14 @@ export class CheckService implements CheckServiceUserCase {
       const req = await fetch(url);
 
       if (!req.ok) throw new Error(`Error on check servie ${url}`);
-
+      const log = new LogEntity(LogLevel.low, `${url} working`);
+      this.logRepository.saveLog(log);
       this.successCallback();
 
       return true;
     } catch (error) {
+      const log = new LogEntity(LogLevel.high, `${url} not working: ${error}`);
+      this.logRepository.saveLog(log);
       this.errorCallback(`${error}`);
       return false;
     }
--- src/presentation/server.ts
+++ src/presentation/server.ts
@@ -1,5 +1,11 @@
 import { CheckService } from "../domain/use-cases/checks/check-service";
+import { FileSystemDataSource } from "../infrastructure/datasoruces/file-system.datasource";
+import { LogRepositoryImpl } from "../infrastructure/repositories/log.repository.impl";
 import { CronService } from "./cron/cron-service";
+
+const fileSystemLogRepository = new LogRepositoryImpl(
+  new FileSystemDataSource(),
+);
 
 export class Server {
   public static run(): void {
@@ -7,6 +13,7 @@ export class Server {
 
     CronService.createJob("*/5 * * * * *", () => {
       new CheckService(
+        fileSystemLogRepository,
         (): void => console.info("Success callback"),
         (error: string): void => console.error(`Error callback ${error}`),
       ).execute("https://www.google.com");
