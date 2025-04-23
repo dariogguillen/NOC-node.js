6d42ae162e1c5df1eb2a3404a0b3a7163a8f29c7
Darío Guillén <guillendario@gmail.com>
2025-04-23 13:13:43 -0600

    feat: email system logs

--- src/presentation/email/email.service.ts
+++ src/presentation/email/email.service.ts
@@ -1,6 +1,7 @@
 import nodemailer from "nodemailer";
 import { envs } from "../../config/plugins/envs.plugins";
 import { Attachment } from "nodemailer/lib/mailer";
+import { logs } from "../../infrastructure/datasoruces/file-system.datasource";
 
 export interface SendMailOptions {
   to: string | string[];
@@ -27,5 +28,23 @@ export class EmailService {
     } catch (error) {
       return false;
     }
+  }
+
+  async sendEmailWithSystemLogs(
+    to: string | string[],
+    from: string,
+  ): Promise<boolean> {
+    const optionsWithLogs: SendMailOptions = {
+      to,
+      from,
+      subject: "System logs",
+      html: "<h1>System logs</h1>",
+      attachments: [
+        { filename: logs.low.filename, path: logs.low.path },
+        { filename: logs.medium.filename, path: logs.medium.path },
+        { filename: logs.high.filename, path: logs.high.path },
+      ],
+    };
+    return await this.sendEmail(optionsWithLogs);
   }
 }
