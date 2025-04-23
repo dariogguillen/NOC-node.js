25ebb54d9cb885c5d6d05a6aff99a52f1e2eae4b
Darío Guillén <guillendario@gmail.com>
2025-04-23 13:13:11 -0600

    refactor: update logs path

--- src/infrastructure/datasoruces/file-system.datasource.ts
+++ src/infrastructure/datasoruces/file-system.datasource.ts
@@ -2,38 +2,38 @@ import fs from "fs";
 import { LogDatasource } from "../../domain/datasources/log.datasource";
 import { LogEntity, LogLevel } from "../../domain/entities/log.entity";
 
-export class FileSystemDataSource implements LogDatasource {
-  private readonly logPath: string = "logs/";
-  private readonly lowLogsPath: string = "logs/logs-low.log";
-  private readonly mediumLogsPath: string = "logs/logs-medium.log";
-  private readonly highLogsPath: string = "logs/logs-high.log";
+export const logs = {
+  path: "logs/",
+  low: { filename: "logs-low.log", path: "logs/logs-low.log" },
+  medium: { filename: "logs-medium.log", path: "logs/logs-medium.log" },
+  high: { filename: "logs-high.log", path: "logs/logs-high.log" },
+};
 
+export class FileSystemDataSource implements LogDatasource {
   constructor() {
     this.createLogsFiles();
   }
 
   private createLogsFiles: () => void = (): void => {
-    if (!fs.existsSync(this.logPath)) {
-      fs.mkdirSync(this.logPath);
+    if (!fs.existsSync(logs.path)) {
+      fs.mkdirSync(logs.path);
     }
-    [this.lowLogsPath, this.mediumLogsPath, this.highLogsPath].forEach(
-      (path) => {
-        if (!fs.existsSync(path)) {
-          fs.writeFileSync(path, "");
-        }
-      },
-    );
+    [logs.low.path, logs.medium.path, logs.high.path].forEach((path) => {
+      if (!fs.existsSync(path)) {
+        fs.writeFileSync(path, "");
+      }
+    });
   };
 
   async saveLog(log: LogEntity): Promise<void> {
     const logAsJson = `${JSON.stringify(log)}\n`;
 
     if (log.level === LogLevel.low) {
-      return fs.appendFileSync(this.lowLogsPath, logAsJson);
+      return fs.appendFileSync(logs.low.path, logAsJson);
     } else if (log.level === LogLevel.medium) {
-      return fs.appendFileSync(this.mediumLogsPath, logAsJson);
+      return fs.appendFileSync(logs.medium.path, logAsJson);
     } else {
-      return fs.appendFileSync(this.highLogsPath, logAsJson);
+      return fs.appendFileSync(logs.high.path, logAsJson);
     }
   }
 
@@ -48,11 +48,11 @@ export class FileSystemDataSource implements LogDatasource {
   async getLog(level: LogLevel): Promise<LogEntity[]> {
     switch (level) {
       case LogLevel.low:
-        return this.getLogsFromFile(this.lowLogsPath);
+        return this.getLogsFromFile(logs.low.path);
       case LogLevel.medium:
-        return this.getLogsFromFile(this.mediumLogsPath);
+        return this.getLogsFromFile(logs.medium.path);
       case LogLevel.high:
-        return this.getLogsFromFile(this.highLogsPath);
+        return this.getLogsFromFile(logs.high.path);
 
       default:
         throw new Error(`${level} level not found`);
